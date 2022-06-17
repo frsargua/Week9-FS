105e6d915d46784ed7114a0c27d6658d092d3a04
frsargua <frsargua@gmail.com>
2022-06-17 17:36:22 +0100

    fixed error "=>"

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const emailValidator = require("email-validator")
+const emailValidator = require("email-validator");
 
 // This is a Welcome message
 
 console.log("Hi, welcome to Node ReadMeGenerator...");
 
-const ReadMeGenerator = (answersArray){
-    `
+const ReadMeGenerator = (answersArray) => {
+  `
 # ${answersArray.title} ![License](https://img.shields.io/badge/${answersArray.license}-License-green)
 
 # Table of Contents
@@ -53,60 +53,60 @@ ${answersArray.Tests}
 Contact me through:
 GitHub: https://github.com/${answersArray.userName}
 Email: ${answersArray.email}
-    `
-}
+    `;
+};
 
-const questions =[
-    {
-        type:"input",
-        name:"title",
-        message:"Please enter your project title:",
-    },
-    {
-        type:"input",
-        name:"description",
-        message:"Please enter your project description",
-    },
-    {
-        type:"input",
-        name:"installation",
-        message:"Please enter your project installation steps:",
-    },
-    {
-        type:"input",
-        name:"usage",
-        message:"Please enter your project Usage",
-    },
-    {
-        type:"list",
-        name:"license",
-        message:"Choose a project license:",
-        choices:["MIT","MIT2","MIT3"]
-    },
-    {
-        type:"input",
-        name:"contributing",
-        message:"Please enter your project contribution guidelines:",
-    },
-    {
-        type:"input",
-        name:"testInstructions",
-        message:"Please enter your project Testing steps:",
-    },
-    {
-        type:"input",
-        name:"userName",
-        message:"Please enter your github name:",
-    },
-    {
-        type:"input",
-        name:"email",
-        message:"Please enter your email address name:",
-    },
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "Please enter your project title:",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please enter your project description",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Please enter your project installation steps:",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please enter your project Usage",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a project license:",
+    choices: ["MIT", "apache-2.0", "afl-3.0"],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Please enter your project contribution guidelines:",
+  },
+  {
+    type: "input",
+    name: "testInstructions",
+    message: "Please enter your project Testing steps:",
+  },
+  {
+    type: "input",
+    name: "userName",
+    message: "Please enter your github name:",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please enter your email address name:",
+  },
 ];
 
-inquirer.prompt(questions).then((response)=>{
-    fs.writeFile("./output/readMe.md", ReadMeGenerator(response),(err) =>
-        err ? console.err(err) : console.log("Success!")
-    );
-})
+inquirer.prompt(questions).then((response) => {
+  fs.writeFile("./output/readMe.md", ReadMeGenerator(response), (err) =>
+    err ? console.err(err) : console.log("Success!")
+  );
+});
