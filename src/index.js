bc6e106a9d2f3a6dc55032de8c5d9c10ee23a4a8
frsargua <frsargua@gmail.com>
2022-06-17 16:55:39 +0100

    added template to index file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,56 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+// This is a Welcome message
+
+console.log("Hi, welcome to Node ReadMeGenerator...");
+
+const ReadMeGenerator = (answersArray){
+    `
+# ${answersArray.title} ![License](https://img.shields.io/badge/${answersArray.license}-License-green)
+
+# Table of Contents
+
+- [Description](#description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [License](#License)
+- [Contributing](#Contributing)
+- [Tests](#Tests)
+- [Questions](#Questions)
+
+## Description
+
+${answersArray.description}
+
+## Installation
+
+Please follow the instructions below:
+
+\`\`\`
+${answersArray.installation}
+\`\`\`
+
+## Usage
+
+${answersArray.usage}
+
+# License
+
+${answersArray.license}
+
+## Contributing
+
+${answersArray.contributing}
+
+## Tests
+
+${answersArray.Tests}
+
+## Questions:
+
+Contact me through:
+GitHub: https://github.com/${answersArray.userName}
+Email: ${answersArray.email}
+    `
+}
